67b4ad7fb45803ad5bb11685534b584f3eae35c4
Eno Akpan <eaugusti@broadinstitute.org>
2021-12-03 09:59:42 -0500

    pushing initial code

---
+++ cmd/yale/go.mod
@@ -0,0 +1,27 @@
+module authenticate/sakeys
+
+go 1.16
+
+require (
+	git.apache.org/thrift.git v0.0.0-20180902110319-2566ecd5d999 // indirect
+	github.com/cpuguy83/go-md2man v1.0.8 // indirect
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
+	github.com/go-delve/delve v1.7.2 // indirect
+	github.com/golang/lint v0.0.0-20180702182130-06c8688daad7 // indirect
+	github.com/gophercloud/gophercloud v0.0.0-20190126172459-c818fa66e4c8 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/openzipkin/zipkin-go v0.1.1 // indirect
+	github.com/pkg/profile v0.0.0-20170413231811-06b906832ed0 // indirect
+	github.com/russross/blackfriday v0.0.0-20180428102519-11635eb403ff // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20211123203042-d83791d6bcd9 // indirect
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
+	google.golang.org/api v0.60.0 // indirect
+	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
+	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	k8s.io/api v0.22.4 // indirect
+	k8s.io/apimachinery v0.22.4 // indirect
+	k8s.io/client-go v0.22.4 // indirect
+	k8s.io/klog v0.3.1 // indirect
+)
---
+++ cmd/yale/yale/yale.go
@@ -0,0 +1,76 @@
+package yale
+
+import (
+	"authenticate/sakeys/client"
+	"authenticate/sakeys/config"
+	"authenticate/sakeys/logs"
+	"golang.org/x/net/context"
+	"google.golang.org/api/iam/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"log"
+)
+
+type Yale struct {
+	config *config.Config       // Yale config
+	gcp    *iam.Service    // GCP Compute API client
+	k8s    kubernetes.Interface // K8s API client
+}
+
+type sakeyInfo struct {
+	privateID string
+}
+
+// NewYale /* Construct a new Yale Manager */
+func NewYale(cfg *config.Config, clients *client.Clients) (*Yale, error) {
+	k8s := clients.GetK8s()
+	gcp := clients.GetGCP()
+
+	return &Yale{cfg, gcp, k8s}, nil
+}
+
+func (m *Yale) Run(){
+	for _, secret := range m.config.SecretData{
+		privateID := m.CreateSAKey(secret.GcpSaName)
+		m.CreateSecret(secret.SecretName, secret.SecretDataKey, secret.Namespace, privateID)
+	}
+}
+
+func ( m *Yale) CreateSecret(SecretName string, SecretKey string, namespace string, privateID string){
+	logs.Info.Printf("Creating secret for %s clients...", SecretName)
+	saKey :=  []byte(privateID)
+	secret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      SecretName,
+		},
+		StringData: map[string]string{
+			"my-key": string(saKey),
+		},
+		Type: v1.SecretTypeOpaque,
+	}
+	secrets, err := m.k8s.CoreV1().Secrets(secret.Namespace).Create(context.TODO(),secret, metav1.CreateOptions{})
+
+	if err != nil {
+		log.Fatal(secrets)
+	}
+}
+
+/*fun updawteKey(){
+//secrets, err := m.k8s.CoreV1().Secrets("default").Get(context.TODO(),SecretName, metav1.GetOptions{})
+//secrets.Data[SecretKey]=  []byte(privateID)
+//m.k8s.CoreV1().Secrets("default").Update(context.TODO(), secrets, metav1.UpdateOptions{})
+}*/
+func (m *Yale)CreateSAKey(GcpSaName string) string {
+	logs.Info.Printf("Creating new SA key for ...", GcpSaName)
+	ctx := context.Background()
+	rb := &iam.CreateServiceAccountKeyRequest{KeyAlgorithm: "KEY_ALG_RSA_1024",
+		PrivateKeyType: "TYPE_GOOGLE_CREDENTIALS_FILE"}
+	newSAKey, err := m.gcp.Projects.ServiceAccounts.Keys.Create(GcpSaName, rb).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return newSAKey.PrivateKeyData
+}
