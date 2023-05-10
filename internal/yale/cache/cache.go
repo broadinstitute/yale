bd1942f150da2ad50a29171cf194fb0db2549d72
Chelsea Hoover <chelsea@broadinstitute.org>
2023-05-10 11:05:51 -0400

    make cache create/read/update abstraction layer

--- internal/yale/cache/cache.go
+++ internal/yale/cache/cache.go
@@ -1,11 +1,13 @@
 package cache
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
+	"github.com/broadinstitute/yale/internal/yale/logs"
 	corev1 "k8s.io/api/core/v1"
-	"regexp"
-	"time"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 // DefaultCacheNamespace default namespace where Yale should store cached service account keys
@@ -21,92 +23,108 @@ const secretKey = "value"
 // prefix for cache entry secret names
 const secretNamePrefix = "yale-cache-"
 
-type Entry struct {
-	// ServiceAccount identifying information for the service account the key belongs to
-	ServiceAccount struct {
-		Email   string // Email for the service account
-		Project string // Project the service account belongs to
+type Cache interface {
+	List() ([]*Entry, error)
+	GetOrCreate(ServiceAccount) (*Entry, error)
+	Save(*Entry) error
+	Delete(*Entry) error
+}
+
+func New(k8s kubernetes.Interface, namespace string) Cache {
+	return &cache{
+		namespace: namespace,
+		k8s:       k8s,
 	}
-	// CurrentKey represents the current/active service account key that will
-	// be replicated to k8s secrets and Vault
-	CurrentKey struct {
-		// JSON representation of the service account key
-		JSON string
-		// ID service account key id
-		ID string
-		// CreatedAt time at which the  service account key was created
-		CreatedAt time.Time
-	}
-	// RotatedKeys map key id -> timestamp representing older versions of the key that were replaced
-	// and should be disabled after a configured amount of time has passed
-	RotatedKeys map[string]time.Time
-	// DisabledKeys map key id -> timestamp representing older versions of the key that were disabled
-	// and should be deleted after a configured amount of time has passed
-	DisabledKeys map[string]time.Time
-	// SyncStatus map used to track sync status for the GcpSaKey resources that use this cache entry.
-	// Each entry in the map describes the last successful sync for a single GcpSaKey resource.
-	// The entry's key is the name of the GcpSaKey, in the form "<namespace>/<name>".
-	// The entry's value contains:
-	// * the checksum of the GcpSaKey's JSON-marshalled spec
-	// * the id of the key that was synced
-	// concatenated with a ":", in the form "<checksum>:<key id>".
-	//
-	// Yale determines if it needs to perform a sync for a particular GcpSaKey by computing this value at runtime.
-	// If the computed value does not match the stored value, it performs a key sync and updates the stored value.
-	//
-	// The advantages of this behavior are:
-	// * If Yale fails to sync a value to Vault due to, say, a permissions issue, it will return an error
-	//   and keep re-trying on every run until the sync succeeds
-	// * If a sync succeeds, Yale will not attempt to sync again until the GcpSaKey's spec changes (say, for example,
-	//   if the key needs to be synced to a different path) or the key is rotated. This avoids overwhelming Vault
-	//   (or eventually Google secrets manager) with write requests.
-	SyncStatus map[string]string
 }
 
-// only lower alphanumeric, ., and - are legal in the names of k8s resources
-var illegalK8sNameCharsRegexp = regexp.MustCompile(`[^a-z0-9.\-]`)
-
-func SecretName(serviceAccountEmail string) string {
-	// replace any characters that are illegal in kubernetes resource names (eg. "@") with "-"
-	normalized := illegalK8sNameCharsRegexp.ReplaceAllString(serviceAccountEmail, "-")
-	// replace anything that's not alphanumeric or . or - with -
-	return secretNamePrefix + normalized
+type cache struct {
+	namespace string
+	k8s       kubernetes.Interface
 }
 
-// Selector returns a label selector that will match all CacheEntries in a namespace
-func Selector() string {
-	return labelKey + "=" + labelValue
-}
-
-func (c *Entry) MarshalToSecret(s *corev1.Secret) error {
-	content, err := json.Marshal(c)
+func (c *cache) List() ([]*Entry, error) {
+	resp, err := c.k8s.CoreV1().Secrets(c.namespace).List(context.Background(), metav1.ListOptions{
+		LabelSelector: labelSelector(),
+	})
 	if err != nil {
-		return fmt.Errorf("error marshalling Entry to JSON: %v", err)
+		return nil, fmt.Errorf("error listing secrets in namespace %s: %v", c.namespace, err)
 	}
-	name := SecretName(c.ServiceAccount.Email)
-	if s.Name == "" {
-		s.Name = name
-	} else if s.Name != name {
-		return fmt.Errorf("error writing Entry to secret - expected name %q, has %q", name, s.Name)
+
+	var entries []*Entry
+	for _, secret := range resp.Items {
+		entry := &Entry{}
+		if err := entry.unmarshalFromSecret(&secret); err != nil {
+			return nil, fmt.Errorf("error unmarshalling cache entry secret %s: %v", secret.Name, err)
+		}
+		entries = append(entries, entry)
 	}
-	if s.Labels == nil {
-		s.Labels = make(map[string]string)
+
+	return entries, nil
+}
+
+func (c *cache) GetOrCreate(sa ServiceAccount) (*Entry, error) {
+	secret, err := c.k8s.CoreV1().Secrets(c.namespace).Get(context.Background(), sa.cacheSecretName(), metav1.GetOptions{})
+	if err != nil {
+		if !errors.IsNotFound(err) {
+			return nil, fmt.Errorf("error checking for existing cache entry for service account %s: %v", sa.Email, err)
+		}
+
+		logs.Info.Printf("secret %s does not exist in cache namespace %s, creating new cache entry for %s", sa.cacheSecretName(), c.namespace, sa.Email)
+		return c.createAndSaveNewEmptyCacheEntry(sa)
 	}
-	s.Labels[labelKey] = labelValue
-	if s.Data == nil {
-		s.Data = make(map[string][]byte)
+
+	var entry Entry
+	err = (&entry).unmarshalFromSecret(secret)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling cache entry secret %s: %v", secret.Name, err)
 	}
-	s.Data[secretKey] = content
+	return &entry, nil
+}
+
+func (c *cache) Save(entry *Entry) error {
+	email := entry.ServiceAccount.Email
+	secretName := entry.ServiceAccount.cacheSecretName()
+
+	secret, err := c.k8s.CoreV1().Secrets(c.namespace).Get(context.Background(), secretName, metav1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("error reading existing cache entry for %s: %v", email, err)
+	}
+	if err = entry.marshalToSecret(secret); err != nil {
+		return fmt.Errorf("error marshalling cache entry for %s to secret: %v", email, err)
+	}
+	_, err = c.k8s.CoreV1().Secrets(c.namespace).Update(context.Background(), secret, metav1.UpdateOptions{})
+	if err != nil {
+		return fmt.Errorf("error updating existing cache entry for %s: %v", email, err)
+	}
 	return nil
 }
 
-func (c *Entry) UnmarshalFromSecret(s *corev1.Secret) error {
-	data, exists := s.Data[secretKey]
-	if !exists {
-		return fmt.Errorf("failed to unmarshal Entry from secret %s (missing %q key)", s.Name, secretKey)
-	}
-	if err := json.Unmarshal(data, c); err != nil {
-		return fmt.Errorf("failed to unmarshal Entry from secret %s: %v", s.Name, err)
+func (c *cache) Delete(entry *Entry) error {
+	if err := c.k8s.CoreV1().Secrets(c.namespace).Delete(context.Background(), entry.ServiceAccount.cacheSecretName(), metav1.DeleteOptions{}); err != nil {
+		return fmt.Errorf("error deleting cache entry secret %s for %s: %v", entry.ServiceAccount.cacheSecretName(), entry.ServiceAccount.Email, err)
 	}
 	return nil
+}
+
+// create a new empty cache entry and save it to the cluster
+func (c *cache) createAndSaveNewEmptyCacheEntry(sa ServiceAccount) (*Entry, error) {
+	logs.Info.Printf("creating new cache entry for %s", sa.Email)
+	entry := newCacheEntry(sa)
+
+	var secret corev1.Secret
+	if err := entry.marshalToSecret(&secret); err != nil {
+		return nil, fmt.Errorf("error marshalling cache entry for %s to secret: %v", sa.Email, err)
+	}
+	logs.Info.Printf("saving new empty cache entry for %s to secret %s in %s", sa.Email, secret.Name, c.namespace)
+	_, err := c.k8s.CoreV1().Secrets(c.namespace).Create(context.Background(), &secret, metav1.CreateOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("error saving cache entry for %s to secret %s in %s: %v", sa.Email, secret.Name, c.namespace, err)
+	}
+
+	return entry, nil
+}
+
+// labelSelector returns a label selector that will match all CacheEntries in a namespace
+func labelSelector() string {
+	return labelKey + "=" + labelValue
 }
---
+++ internal/yale/cache/cache_test.go
@@ -0,0 +1,185 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/broadinstitute/yale/internal/yale/testutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"testing"
+	"time"
+)
+
+const namespace = "my-cache-namespace"
+const project = "my-project"
+
+var sa1 = ServiceAccount{
+	Email:   "my-sa1@p.com",
+	Project: project,
+}
+var sa2 = ServiceAccount{
+	Email:   "my-sa2@p.com",
+	Project: project,
+}
+var sa3 = ServiceAccount{
+	Email:   "my-sa3@p.com",
+	Project: project,
+}
+
+func Test_Cache(t *testing.T) {
+	k8s := testutils.NewFakeK8sClient(t)
+	cache := New(k8s, namespace)
+
+	// make sure secret does not exist, to start
+	secret := readCacheSecret(t, k8s, sa1.cacheSecretName())
+	require.Nil(t, secret)
+
+	// make sure we get an empty list
+	entries, err := cache.List()
+	require.NoError(t, err)
+	assert.Len(t, entries, 0)
+
+	// create new empty cache entry
+	expected := emptyCacheEntry(sa1)
+	entry, err := cache.GetOrCreate(sa1)
+	require.NoError(t, err)
+	assert.Equal(t, &expected, entry)
+
+	// make sure the underlying secret was created with the attributes we expect
+	secret = readCacheSecret(t, k8s, sa1.cacheSecretName())
+	require.NotNil(t, secret)
+	expectedContent, err := json.Marshal(expected)
+	require.NoError(t, err)
+	assert.Equal(t, sa1.cacheSecretName(), secret.Name)
+	assert.Equal(t, namespace, secret.Namespace)
+	assert.Equal(t, labelValue, secret.Labels[labelKey])
+	assert.Equal(t, string(expectedContent), string(secret.Data[secretKey]))
+
+	// reading the entry again should yield a copy of the entry with identical data
+	entryCopy, err := cache.GetOrCreate(sa1)
+	require.NoError(t, err)
+	assert.Equal(t, entry, entryCopy)
+
+	now := time.Now().Round(0)
+
+	// updating and saving entry should persist the changes
+	entry.CurrentKey.ID = "key-1"
+	entry.CurrentKey.CreatedAt = now
+	entry.CurrentKey.JSON = `{"foo":"bar"}`
+
+	entry.RotatedKeys["key-2"] = now
+	entry.RotatedKeys["key-3"] = now
+	entry.DisabledKeys["key-4"] = now
+	entry.SyncStatus["my-ns/my-gsk"] = "my-sha256-sum:key-1"
+
+	require.NoError(t, cache.Save(entry))
+
+	// make sure saving the cache entry did not overwrite any of the fields we set on the entry object
+	assert.Equal(t, "key-1", entry.CurrentKey.ID)
+	assert.Equal(t, now, entry.CurrentKey.CreatedAt)
+	assert.Equal(t, `{"foo":"bar"}`, entry.CurrentKey.JSON)
+	assert.Equal(t, now, entry.RotatedKeys["key-2"])
+	assert.Equal(t, now, entry.RotatedKeys["key-3"])
+	assert.Equal(t, now, entry.DisabledKeys["key-4"])
+	assert.Equal(t, "my-sha256-sum:key-1", entry.SyncStatus["my-ns/my-gsk"])
+
+	// reading the entry again should yield a copy of the entry with identical data
+	entryCopy, err = cache.GetOrCreate(sa1)
+	require.NoError(t, err)
+	assert.Equal(t, entry, entryCopy)
+
+	// listing all cache entries should yield just the entry we created
+	entries, err = cache.List()
+	require.NoError(t, err)
+	assert.Len(t, entries, 1)
+	assert.Equal(t, entry, entries[0])
+
+	// add 2 more cache entries
+	entry2, err := cache.GetOrCreate(sa2)
+	require.NoError(t, err)
+	assert.Equal(t, emptyCacheEntry(sa2), *entry2)
+
+	entry3, err := cache.GetOrCreate(sa3)
+	require.NoError(t, err)
+	assert.Equal(t, emptyCacheEntry(sa3), *entry3)
+
+	// make sure updates to entry3 persist
+	entry3.CurrentKey.ID = "e3-key3"
+	require.NoError(t, cache.Save(entry3))
+
+	entry3Copy, err := cache.GetOrCreate(sa3)
+	require.NoError(t, err)
+	assert.Equal(t, "e3-key3", entry3Copy.CurrentKey.ID)
+
+	// make sure all entries appear in the list
+	entries, err = cache.List()
+	assert.Len(t, entries, 3)
+	assert.Equal(t, entry, entries[0])
+	assert.Equal(t, entry2, entries[1])
+	assert.Equal(t, entry3, entries[2])
+
+	// delete entry2
+	require.NoError(t, cache.Delete(entry2))
+	entries, err = cache.List()
+
+	// make sure entry and entry3 appear in the list
+	assert.Len(t, entries, 2)
+	assert.Equal(t, entry, entries[0])
+	assert.Equal(t, entry3, entries[1])
+
+	// delete first entry
+	require.NoError(t, cache.Delete(entry))
+
+	// make sure just entry3 appears in the list
+	entries, err = cache.List()
+	assert.Len(t, entries, 1)
+	assert.Equal(t, entry3, entries[0])
+
+	// delete entry3
+	require.NoError(t, cache.Delete(entry3))
+
+	// make sure list is empty again
+	entries, err = cache.List()
+	assert.Len(t, entries, 0)
+
+	// get or create new entry for the same sa as a deleted entry should create a new empty entry
+	entry, err = cache.GetOrCreate(sa1)
+	require.NoError(t, err)
+	assert.Equal(t, emptyCacheEntry(sa1), *entry)
+}
+
+func Test_cacheSecretName(t *testing.T) {
+	assert.Equal(t, "yale-cache-my-sa-my-project.com", sa1.cacheSecretName())
+}
+
+func readCacheSecret(t *testing.T, k8s kubernetes.Interface, name string) *corev1.Secret {
+	secret, err := k8s.CoreV1().Secrets(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		return nil
+	}
+	require.NoError(t, err)
+	return secret
+}
+
+// represents the expected initial state of a new/empty cache entry
+func emptyCacheEntry(sa ServiceAccount) Entry {
+	return Entry{
+		ServiceAccount: ServiceAccount{
+			Email:   sa.Email,
+			Project: sa.Project,
+		},
+		CurrentKey: struct {
+			JSON      string
+			ID        string
+			CreatedAt time.Time
+		}{},
+		// we expect _empty_ maps, not nil maps
+		RotatedKeys:  map[string]time.Time{},
+		DisabledKeys: map[string]time.Time{},
+		SyncStatus:   map[string]string{},
+	}
+}
